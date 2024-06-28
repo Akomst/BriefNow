a98be628ededc54aa56d959f789a08cc8a3b3bd4
akomst <akomst6@gmail.com>
2024-06-28 20:45:12 +0000

    updated config file to include more news sources and reorganized the sources

--- config.py
+++ config.py
@@ -7,16 +7,54 @@ rss_feeds = {
     "Reuters": "http://feeds.reuters.com/reuters/topNews",
     "The Guardian": "https://www.theguardian.com/world/rss",
     "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
-    "New York Times": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+    "New York Times": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "The Guardian": "https://guardian.ng/feed/",
+    "Premium Times": "https://www.premiumtimesng.com/feed",
+    "Punch": "https://punchng.com/feed/",
+    "ThisDay": "https://www.thisdaylive.com/index.php/feed/",
+    "Leadership": "https://leadership.ng/feed/",
+    "Tribune": "https://tribuneonlineng.com/feed/",
+    "DailyNigeria": "https://dailynigerian.com/feed/",
+    "The Herald": "https://www.herald.ng/feed/",
+    "The Gazette": "https://thegazettengr.com/feed/",
+    "Vanguard": "https://www.vanguardngr.com/feed/",
+    "The Nation": "https://thenationonlineng.net/feed",
+    "Sahara Reporters": "https://saharareporters.com/articles/rss-feed",
+    "Yahoo": "http://rss.news.yahoo.com/rss/world",
+    "BBC India":"http://feeds.bbci.co.uk/news/world/asia/india/rss.xml ",
+    "The Hindu": "http://www.thehindu.com/news/national/?service=rss",
+    "Times of India": "http://timesofindia.feedsportal.com/c/33039/f/533965/index.rss "
 }
+
 
 # Mapping categories to RSS feeds
 category_feeds = {
-    "Sports": ["BBC", "CNN"],
-    "Entertainment": ["Reuters", "The Guardian"],
-    "Politics": ["Al Jazeera", "New York Times"],
-    "Business": ["Reuters", "New York Times"],
-    "Local": ["The Guardian"],
-    "Regional": ["Al Jazeera"],
-    "Global": ["BBC", "CNN"]
+    "Nigeria":[
+    "The Guardian",
+     "Premium Times", 
+     "Punch", 
+     "ThisDay",
+      "Leadership", 
+      "Tribune",
+      "DailyNigeria", 
+      "The Herald",
+      "The Gazette",
+      "Vanguard", 
+      "The Nation",
+      "Sahara Reporters"],
+      
+    "Global": [
+    "Al Jazeera", 
+    "BBC", 
+    "Yahoo",
+    "CNN", 
+    "The Guardian", 
+    "New York Times",  
+    "Guardian"],
+    
+    "India News": [
+    "BBC India",
+    "Times of India", 
+    "The Hindu"]
+    
 }
